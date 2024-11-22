3189ec0a00a96ed7fb66b36123a96b688d62f2cd
Yassine <m_djeziri@estin.dz>
2024-11-22 01:32:46 +0100

    setting up docker files and django , postgres req

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from the Frontend!</h1>
+      <p>This is a dummy frontend app running in a Docker container.</p>
+    </div>
+  );
+}
+
+export default App;
